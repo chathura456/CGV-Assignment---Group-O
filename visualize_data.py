778f3307ba0931fbf9dbd964bbcc4594fc3b4943
dmldperera <dmldperera@students.nsbm.ac.lk>
2024-09-09 18:23:48 +0530

    Get File Path

--- visualize_data.py
+++ visualize_data.py
@@ -1,3 +1,9 @@
 import matplotlib.pyplot as plt
 import os
 import ast
+
+
+# Function to get the absolute path of a file based on the script's location
+def get_file_path(filename):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, filename)
